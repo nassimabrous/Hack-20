40b6c12e2f4a808faa0a6367a8539bc4be79c655
Henry Heino <personalizedrefrigerator@gmail.com>
2020-08-15 19:47:38 -0700

    Account sign-in and style fixes

--- popups/annotate.js
+++ popups/annotate.js
@@ -1,45 +1,16 @@
 "use strict";
 
-// API Keys!
-var firebaseConfig = 
-{
-    apiKey: "AIzaSyBfwKKUrHKnfkB70blICCZ3X13qxYLq6-I",
-    authDomain: "coannote.firebaseapp.com",
-    databaseURL: "https://coannote.firebaseio.com",
-    projectId: "coannote",
-    storageBucket: "coannote.appspot.com",
-    messagingSenderId: "1049937372643",
-    appId: "1:1049937372643:web:e8493165df8f768812acd6"
-};
-
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-CloudHelper.initDB(firebase, 
-{
-    apiData: firebaseConfig,
-    resources: [ CloudHelper.Service.FIRESTORE, CloudHelper.Service.FIREBASE_STORAGE ]
-});
-
 function main()
 {
     let button = document.querySelector("#mainButton");
 
     button.addEventListener("click", () =>
     {
-        chrome.tabs.executeScript(null, {file: "/Libs/firebase/firebaseInit.js"}, function()
-        {
-            chrome.tabs.executeScript(null, {file: "/Libs/LibJS/FullLibJS.js"});
-            chrome.tabs.executeScript(null, {file: "/content_scripts/authenticate.js"});
-            chrome.tabs.executeScript(null, {file: "/content_scripts/annotate.js"});
-
-            window.close();
-        });
+        chrome.tabs.executeScript(null, {file: "/content_scripts/annotate.js"});
+        window.close();
     });
 
     button.focus();
-
-    JSHelper.Notifier.notify(JSHelper.GlobalEvents.PAGE_SETUP_COMPLETE);
-    AuthHelper.insertAuthCommands(document.body);
 }
 
 requestAnimationFrame(main); // Defer running main...
