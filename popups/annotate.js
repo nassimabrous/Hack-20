9cd2af5cbbc0918063fd7df6cf4c4738ca224754
Henry Heino <personalizedrefrigerator@gmail.com>
2020-08-15 19:33:54 -0700

    Updates to annotate.js

--- popups/annotate.js
+++ popups/annotate.js
@@ -14,6 +14,11 @@ var firebaseConfig =
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+CloudHelper.initDB(firebase, 
+{
+    apiData: firebaseConfig,
+    resources: [ CloudHelper.Service.FIRESTORE, CloudHelper.Service.FIREBASE_STORAGE ]
+});
 
 function main()
 {
@@ -38,3 +43,32 @@ function main()
 }
 
 requestAnimationFrame(main); // Defer running main...
+
+/* ------------------------------ Helper Functions  ------------------------------ */
+
+/**
+ * Returns the element that has the ID attribute with the specified value.
+ * @param {string} idName - element ID
+ * @returns {object} DOM object associated with id.
+ */
+function id(idName) {
+    return document.getElementById(idName);
+}
+
+/**
+ * Returns the first element that matches the given CSS selector.
+ * @param {string} query - CSS query selector.
+ * @returns {object} The first DOM object matching the query.
+ */
+function qs(query) {
+    return document.querySelector(query);
+}
+
+/**
+ * Returns the array of elements that match the given CSS selector.
+ * @param {string} query - CSS query selector
+ * @returns {object[]} array of DOM objects matching the query.
+ */
+function qsa(query) {
+    return document.querySelectorAll(query);
+}
